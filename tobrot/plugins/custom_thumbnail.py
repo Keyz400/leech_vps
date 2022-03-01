bb5d4b348a625c043e876383fb1864a845f0cbf8
Anshu Sharma <97522397+Anshusharma75@users.noreply.github.com>
2022-03-01 12:43:57 +0530

    Update custom_thumbnail.py

--- tobrot/plugins/custom_thumbnail.py
+++ tobrot/plugins/custom_thumbnail.py
@@ -1,26 +1,31 @@
-"""ThumbNail utilities, © @AnyDLBot"""
-
+  #ThumbNail utilities, © @AnyDLBot
 
 import os
+
+from tobrot import DOWNLOAD_LOCATION
 
 from hachoir.metadata import extractMetadata
 from hachoir.parser import createParser
 from PIL import Image
-from tobrot import DOWNLOAD_LOCATION
 
 
 async def save_thumb_nail(client, message):
-    thumbnail_location = os.path.join(DOWNLOAD_LOCATION, "thumbnails")
-    thumb_image_path = os.path.join(
-        thumbnail_location, str(message.from_user.id) + ".jpg"
+    thumbnail_location = os.path.join(
+        DOWNLOAD_LOCATION,
+        "thumbnails"
     )
-    ismgs = await message.reply_text("<code>Processing . . . 🔄</code>")
+    thumb_image_path = os.path.join(
+        thumbnail_location,
+        str(message.from_user.id) + ".jpg"
+    )
+    ismgs = await message.reply_text("Analyzing the image...")
     if message.reply_to_message is not None:
         if not os.path.isdir(thumbnail_location):
             os.makedirs(thumbnail_location)
         download_location = thumbnail_location + "/"
         downloaded_file_name = await client.download_media(
-            message=message.reply_to_message, file_name=download_location
+            message=message.reply_to_message,
+            file_name=download_location
         )
         # https://stackoverflow.com/a/21669827/4723940
         Image.open(downloaded_file_name).convert("RGB").save(downloaded_file_name)
@@ -38,20 +43,23 @@ async def save_thumb_nail(client, message):
         # https://pillow.readthedocs.io/en/3.1.x/reference/Image.html#create-thumbnails
         os.remove(downloaded_file_name)
         await ismgs.edit(
-            "<b>⚡<i>Custom Thumbnail 🖼 Saved for Next Uploads</i>⚡</b>\n\n"
-            + "<b><i>✅Your Photo is Set, Ready to Go ...👨‍🦯</i></b>."
+            "Your image has been saved. " + \
+            "This image will be used as video thumbnail, till /clearthumbnail."
         )
     else:
-        await ismgs.edit("<b><i>⛔Sorry⛔</i></b>\n\n" + "<b>❌ Reply with Image to Save Your Custom Thumbnail.❌</b>")
+        await message.edit("Reply to a photo to save custom thumbnail")
+
 
 async def clear_thumb_nail(client, message):
-    thumbnail_location = os.path.join(DOWNLOAD_LOCATION, "thumbnails")
-    thumb_image_path = os.path.join(
-        thumbnail_location, str(message.from_user.id) + ".jpg"
+    thumbnail_location = os.path.join(
+        DOWNLOAD_LOCATION,
+        "thumbnails"
     )
-    ismgs = await message.reply_text("<code>Processing . . . 🔄</code>")
+    thumb_image_path = os.path.join(
+        thumbnail_location,
+        str(message.from_user.id) + ".jpg"
+    )
+    ismgs = await message.reply_text("Searching your thumbnail ...")
     if os.path.exists(thumb_image_path):
         os.remove(thumb_image_path)
-        await ismgs.edit("<b><i>✅Success✅</i></b>\n\n" + "<b>🖼Custom Thumbnail Cleared Successfully As Per Your Request.</b>")
-    else:
-        await ismgs.edit("<b><i>⛔Sorry⛔</i></b>\n\n" + "<b>❌Nothing to Clear For You❌</b>")
+    await ismgs.edit("✅ Custom thumbnail has been cleared .")
